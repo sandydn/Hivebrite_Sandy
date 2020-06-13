2c79d762eee0f19507bf29f89d397f30cd4a572c
sandydn <sandydn78@gmail.com>
2020-06-13 17:24:24 +0200

    feat(react): initialize project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: 40px auto;
+  width: 1000px;
+`
+
+const App = () => (
+  <Wrapper>
+    Hi !
+  </Wrapper>
+)
+
+export default App;
